35ae0f8f8728ec8eb66ce43ee5d70dca26c0fb05
Martin Kleppe <kleppe@ubilabs.net>
2010-05-21 13:36:53 +0200

    Toggle link added to see sticky footer behaviour.

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,6 +1,12 @@
 (function($, window, undefined){
   $(document).ready(function(){
     // call your code here
+    
+    $("#toggle").click(function(){
+       $(".section").toggle();
+      return false;
+    });
+    
   });
 })(jQuery, window);
 
